0d31310cda0ccb48a9b0964f99c6f5302c2df59d
İsmail Bilener <44755411+ismailbilener@users.noreply.github.com>
2022-09-06 20:15:16 +0300

    Add files via upload

---
+++ Asalsayilar/src/Asalsayi.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Asalsayi {
+    static void asal(int sayi,int i){
+        if (i == sayi) {
+            System.out.print(sayi + " asal sayıdır.");
+            return;
+        } else if (sayi%i == 0) {
+            System.out.print(sayi + " asal sayı değildir.");
+            return;
+        }
+
+        asal(sayi, i + 1);
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı giriniz:  ");
+        int sayi=input.nextInt();
+        asal(sayi,2);
+
+    }
+}
